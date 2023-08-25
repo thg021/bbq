5eb3a7ccf471721e0ba6ede12b9a6847f0d53601
Thiago Silva <tsilva6@uolinc.com>
2023-08-25 16:19:24 -0300

    feat: adicionado card do bbq agendado

--- src/app/(pages)/schedule/page.tsx
+++ src/app/(pages)/schedule/page.tsx
@@ -1,40 +1,304 @@
+import Image from "next/image"
+import peopleIcon from "../../../../public/img/people.svg"
+import moneyIcon from "../../../../public/img/money.svg"
+import bbqIcon from "../../../../public/img/bbq.svg"
 export default function Schedule() {
   return (
     <main className="w-full flex-1 flex justify-start items-center flex-col bg-slate-100">
-      <div className="w-full lg:w-[64rem] bg-slate-100 flex-1">
-        <section className="grid grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-4 mt-[-3rem] px-4">
-          <div className="flex-1 flex flex-col bg-white shadow-md p-6 h-48">
+      <div className="w-full lg:w-[64rem] flex-1 mt-[-3rem]">
+        <section className="grid grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-4 px-4">
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
             <header className="font-extrabold text-3xl">01/12</header>
             <span className="flex-1 font-bold text-xl">Niver do GUI</span>
 
             <footer className="flex justify-between font-medium text-xl">
-              <div>
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
                 <span>12</span>
               </div>
-              <div>R$ 140,00</div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
             </footer>
           </div>
-          <div className="flex-1 flex flex-col bg-white shadow-md p-6 h-48">
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
             <header className="font-extrabold text-3xl">01/12</header>
             <span className="flex-1 font-bold text-xl">Niver do GUI</span>
 
             <footer className="flex justify-between font-medium text-xl">
-              <div>
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
                 <span>12</span>
               </div>
-              <div>R$ 140,00</div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
             </footer>
           </div>
-          <div className="flex-1 flex flex-col bg-white shadow-md p-6 h-48">
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
             <header className="font-extrabold text-3xl">01/12</header>
             <span className="flex-1 font-bold text-xl">Niver do GUI</span>
 
             <footer className="flex justify-between font-medium text-xl">
-              <div>
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
                 <span>12</span>
               </div>
-              <div>R$ 140,00</div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
             </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col bg-white shadow-md p-6 h-48">
+            <header className="font-extrabold text-3xl">01/12</header>
+            <span className="flex-1 font-bold text-xl">Niver do GUI</span>
+
+            <footer className="flex justify-between font-medium text-xl">
+              <div className="flex justify-center items-center gap-3">
+                <Image src={peopleIcon} alt="" />
+                <span>12</span>
+              </div>
+
+              <div className="flex justify-center items-center gap-3">
+                <Image src={moneyIcon} alt="" />
+                <span>R$ 140,00</span>
+              </div>
+            </footer>
+          </div>
+
+          <div className="flex flex-col justify-center items-center gap-2 bg-slate-100 shadow-md p-6 h-48">
+            <Image src={bbqIcon} alt="" />
+            <h1 className="font-bold text-xl">Adicionar Churras</h1>
           </div>
         </section>
       </div>
