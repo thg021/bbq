0a0143b3ee450b6874493965f5f8beea3e290ca5
Thiago Silva <tsilva6@uolinc.com>
2023-08-26 22:12:33 -0300

    refactor: adicionado forwardRef para input

--- src/app/components/Input/index.tsx
+++ src/app/components/Input/index.tsx
@@ -1,24 +1,29 @@
-import { InputHTMLAttributes } from "react"
+import { InputHTMLAttributes, forwardRef } from "react"
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string
   name: string
 }
 
-export function Input({ label, name, type, ...rest }: InputProps) {
-  return (
-    <div className="flex flex-col space-y-3">
-      {!!label && (
-        <label className="font-bold text-xl" htmlFor={name}>
-          {label}
-        </label>
-      )}
-      <input
-        className="px-4 py-3 rounded-sm placeholder:italic placeholder:text-slate-600"
-        id={name}
-        type={type}
-        {...rest}
-      />
-    </div>
-  )
-}
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, name, type, ...rest }, ref) => {
+    return (
+      <div className="flex flex-col space-y-3">
+        {!!label && (
+          <label className="font-bold text-xl" htmlFor={name}>
+            {label}
+          </label>
+        )}
+        <input
+          className="px-4 py-3 rounded-sm placeholder:italic placeholder:text-slate-600"
+          id={name}
+          type={type}
+          ref={ref}
+          {...rest}
+        />
+      </div>
+    )
+  }
+)
+
+Input.displayName = "Input"
